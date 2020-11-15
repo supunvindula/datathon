330a84bdd3a0c381f1338ae2b0353b93e121c4cc
supunvindula <e16377@eng.pdn.ac.lk>
2020-11-15 22:09:56 +0530

    site added

---
+++ src/Components/Header.js
@@ -0,0 +1,62 @@
+import React, { PropTypes, Component } from 'react';
+import ParticlesBg  from "particles-bg";
+
+class Header extends Component {
+  render() {
+
+    if(this.props.data){
+       var project = this.props.data.project;
+       var github = this.props.data.github;
+      var name = this.props.data.name;
+      var description= this.props.data.description;
+      var city= this.props.data.address.city;
+      var logo= "images/"+this.props.data.logo;
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <header id="home" style={{"height":"847.8px"}}>
+      {/* <ParticlesBg color="#034460" type="circle" bg={true} /> */}
+      {/* <ParticlesBg color="#EE7E4E" type="circle" bg={true} /> */}
+      <ParticlesBg type="circle" bg={true} />
+      <nav id="nav-wrap">
+         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" className="nav">
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            <li><a className="smoothscroll" href="#about">Rules</a></li>
+	         <li><a className="smoothscroll" href="#resume">Prizes</a></li>
+            {/* <li><a className="smoothscroll" href="#portfolio">Winners</a></li> */}
+            <li><a className="smoothscroll" href="#contact">Sponsers</a></li>
+         </ul>
+      </nav>
+
+      <div className="row banner">
+      
+         <div className="banner-text">
+            {/* <h1 className="responsive-headline">{name}</h1> */}
+            <img className="profile-pic"  src={logo} alt="DATATHON 2020" />
+            <h3>{description}.</h3>
+            <br/>
+            <hr />
+            <ul className="social">
+               <a href={project} target="_blank" className="button btn project-btn"><i className="fa fa-book"></i>Register Now!</a>
+               {/* <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a> */}
+            </ul>
+            <hr />
+         </div>
+         <br/>
+      </div>
+      <p className="scrolldown">
+         <a className="smoothscroll" href="#countdown"><i className="icon-down-circle"></i></a>
+      </p>
+
+   </header>
+    );
+  }
+}
+
+export default Header;
