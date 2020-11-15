e00a1a837f05e4ab9aa91bd1a27f59d606e00fe5
supunvindula <e16377@eng.pdn.ac.lk>
2020-11-15 22:39:19 +0530

    rotes removed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render((
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
- ),<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
