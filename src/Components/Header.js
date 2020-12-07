0d14af43a16d41a91de81baae7398ec689be279f
Shirly Madushanka <shirlyekanayake@gmail.com>
2020-12-07 12:39:53 +0530

    description changed and animations added.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,26 @@
 import React, { PropTypes, Component } from 'react';
 import ParticlesBg from "particles-bg";
 import Countdown from './Countdown';
+import AOS from 'aos';
 
 class Header extends Component {
+   componentDidMount() {
+      // or simply just AOS.init();
+      AOS.init({
+         // initialise with other settings
+         duration: 2000
+      });
+   }
    render() {
       const currentDate = new Date();
       const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
 
       if (this.props.data) {
          var project = this.props.data.project;
-         var description = this.props.data.description;
+         var organizedBy = this.props.data.organizedBy;
+         var bio = this.props.data.bio;
          var logo = "images/" + this.props.data.logo;
-        
+
       }
 
       return (
@@ -36,14 +45,15 @@ class Header extends Component {
 
                <div className="banner-text">
                   {/* <h1 className="responsive-headline">{name}</h1> */}
-                  <img className="profile-pic" src={logo} alt="DATATHON 2020" />
-                  <h3>{description}.</h3>
-                  <br />
+                  <img className="profile-pic" src={logo} alt="DATATHON 2020" data-aos="fade-in" />
+                  <h3 data-aos="zoom-in-down">Organized by ACES</h3>
+                  <h3 data-aos="zoom-in-left">{organizedBy}</h3>
+                  <h3 data-aos="zoom-in-right">{bio}</h3>
                   <hr />
-                  <ul className="social">
+                  <ul className="social" data-aos="fade-left">
                      <Countdown date={`${year}-12-24T00:00:00`} />
                   </ul>
-                  <ul className="social">
+                  <ul className="social bounce" data-aos="fade-right">
                      <a href={project} target="_blank" className="button btn project-btn"><i className="fa fa-book"></i>Register Now!</a>
                      {/* <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a> */}
                   </ul>
